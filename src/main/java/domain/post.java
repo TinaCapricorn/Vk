ed2e6cb4127954000cd8c56630dcc5af4a4209ac
Klein Kristina <klein281293@gmail.com>
2022-01-08 10:01:59 +0700

    Changes

--- src/main/java/domain/post.java
+++ src/main/java/domain/post.java
@@ -4,6 +4,19 @@ public class post {
 
     private int id;
     private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private String postType;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    public int isPinned;
+    public int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
     // + get/set на все поля
 
     public class header{
@@ -15,16 +28,39 @@ public class post {
 
     public class body{
         private String text;
-        private String postMedia;
         private String postShare;
+
+        public class geo{
+            private String typeGeo;
+            private String coordinate;
+        }
         // + get/set на все поля
     }
 
     public class footer{
-        private int countLike;
-        private int countComment;
-        private int countShare;
-        private int countView;
+        public class comment{
+            public int countComment;
+            private int canPost;
+            private int groupCanPost;
+            private boolean canOpen;
+            private boolean canClose;
+        }
+
+        public class like{
+            public int countLike;
+            public int authorLike;
+            private int canLike;
+            private int canPublish;
+        }
+
+        public class repost{
+            public int countRepost;
+            private int authorReposted;
+        }
+
+        public class view{
+            public int countView;
+        }
         // + get/set на все поля
     }
 }
